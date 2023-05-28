3f4a30a8ee94523e1053cc8663c7fd1e311c4626
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-05-28 22:03:07 +0530

    Time: 5 ms (59.72%), Space: 42 MB (99.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             char sc =s.charAt(i);
+//             char tc =t.charAt(i);
+//             map.put(sc,map.getOrDefault(sc,0)+1);
+//             map.put(tc,map.getOrDefault(tc,0)-1);
+//         }
+        
+//         for(int i:map.values()){
+//             if(i!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        //Using Array
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Integer> map = new HashMap<>();
+        int[] arr= new int[26];
         for(int i=0;i<s.length();i++){
-            char sc =s.charAt(i);
-            char tc =t.charAt(i);
-            map.put(sc,map.getOrDefault(sc,0)+1);
-            map.put(tc,map.getOrDefault(tc,0)-1);
+            arr[s.charAt(i)-'a']++;
         }
-        
-        for(int i:map.values()){
-            if(i!=0){
+        for(int i=0;i<t.length();i++){
+            arr[t.charAt(i)-'a']--;
+            if(arr[t.charAt(i)-'a']<0){
                 return false;
             }
         }
         return true;
+        
     }
 }
