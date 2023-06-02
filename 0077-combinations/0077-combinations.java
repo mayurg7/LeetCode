b5badaf37ae7f63fcb9227245727505000dbbf30
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-06-02 18:18:00 +0530

    Time: 26 ms (40.20%), Space: 45.2 MB (26.65%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,26 +1,42 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        int a = 1;
-        int[] nums = new int[n];
-        for(int i=0;i<n;i++){
-            nums[i]=a;
-            a++;
-        }
+//         int a = 1;
+//         int[] nums = new int[n];
+//         for(int i=0;i<n;i++){
+//             nums[i]=a;
+//             a++;
+//         }
+//         List<List<Integer>> result = new ArrayList<>();
+//         Comb(result,new ArrayList<>(),nums,0,k);
+//         return result;
+//     }
+    
+//     private void Comb(List<List<Integer>> ans,List<Integer> temp , int[] nums,int start, int k){
+//         if(temp.size()==k){
+//             ans.add(new ArrayList<>(temp));
+//             return;
+//         }
+        
+//         for(int i=start;i<nums.length;i++){
+//             temp.add(nums[i]);
+//             Comb(ans,temp,nums,i+1,k);
+//             temp.remove(temp.size()-1);
+//         }
+//     }
+        
         List<List<Integer>> result = new ArrayList<>();
-        Comb(result,new ArrayList<>(),nums,0,k);
+        GenerateComb(result,new ArrayList<>(),1,n,k);
         return result;
     }
-    
-    private void Comb(List<List<Integer>> ans,List<Integer> temp , int[] nums,int start, int k){
-        if(temp.size()==k){
-            ans.add(new ArrayList<>(temp));
+        private static void GenerateComb(List<List<Integer>> ans,List<Integer> curr , int start, int n , int k ){
+        if(curr.size()==k){
+            ans.add(new ArrayList<>(curr));
             return;
         }
-        
-        for(int i=start;i<nums.length;i++){
-            temp.add(nums[i]);
-            Comb(ans,temp,nums,i+1,k);
-            temp.remove(temp.size()-1);
+        for(int i=start;i<=n;i++){
+            curr.add(i);
+            GenerateComb(ans,curr,i+1,n,k);
+            curr.remove(curr.size()-1);
         }
     }
 }
