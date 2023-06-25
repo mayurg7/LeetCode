7f640bb4d22c37ea582a289d9b2124f6d9e62ce6
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-06-26 00:01:14 +0530

    Time: 16 ms (33.04%), Space: 47.3 MB (23.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length==0 || strs==null){
-            return new ArrayList<>();
-        }
         
         HashMap<String,List<String>> map = new HashMap<>();
         
-        
-        for(String i:strs){
-            
+        for(String i : strs){
             String freq = getFreq(i);
             
             if(map.containsKey(freq)){
                 map.get(freq).add(i);
             }else{
-                List<String> strList = new ArrayList<>();
-                strList.add(i);
-                map.put(freq,strList);
+                List<String> strlist = new ArrayList<>();
+                strlist.add(i);
+                map.put(freq,strlist);
             }
+            
         }
         return new ArrayList<>(map.values());
+        
     }
     
-    
-    private String getFreq(String str){
+    private static String getFreq(String s){
+        StringBuilder sb = new StringBuilder();
+        
         int[] freq = new int[26];
         
-        for(char c: str.toCharArray()){
-            freq[c-'a']++;
+        for(char i : s.toCharArray()){
+            freq[i-'a']++;
         }
-        StringBuilder result = new StringBuilder();
+        
         char c = 'a';
-        for(int i:freq){
-            result.append(c);
-            result.append(i);
+        
+        for(int i : freq){
+            sb.append(c);
+            sb.append(i);
             c++;
         }
-        return result.toString();
+        return sb.toString();
     }
 }
