c2767c8fe005dfaf59aaeb5285984e758f2a52fe
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-06-25 21:32:16 +0530

    Time: 11 ms (59.92%), Space: 41.5 MB (82.61%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()){
+        if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character, Character> map = new HashMap<>();
-        
-        for(int i=0;i<s.length();i++){
-            
+        HashMap<Character,Character> map = new HashMap<>();
+
+        for(int i = 0;i<s.length();i++){
             if(!map.containsKey(s.charAt(i))){
                 if(!map.containsValue(t.charAt(i))){
                     map.put(s.charAt(i),t.charAt(i));
@@ -14,12 +13,12 @@ class Solution {
                     return false;
                 }
             }else{
-                Character sc = map.get(s.charAt(i));
-                if(sc!=t.charAt(i)){
+                char sc = map.get(s.charAt(i));
+                if(sc != t.charAt(i)){
                     return false;
                 }
             }
         }
-        return true;
+        return true;        
     }
 }
