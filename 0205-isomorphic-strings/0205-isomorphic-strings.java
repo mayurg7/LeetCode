dd098819738a42dd28180732a80d4bf25b61c443
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-06-25 21:32:30 +0530

    Time: 11 ms (59.92%), Space: 41.3 MB (95.30%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()){
+        if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Character> map = new HashMap<>();
-
-        for(int i = 0;i<s.length();i++){
+        HashMap<Character, Character> map = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            
             if(!map.containsKey(s.charAt(i))){
                 if(!map.containsValue(t.charAt(i))){
                     map.put(s.charAt(i),t.charAt(i));
@@ -13,12 +14,12 @@ class Solution {
                     return false;
                 }
             }else{
-                char sc = map.get(s.charAt(i));
-                if(sc != t.charAt(i)){
+                Character sc = map.get(s.charAt(i));
+                if(sc!=t.charAt(i)){
                     return false;
                 }
             }
         }
-        return true;        
+        return true;
     }
 }
