4f9f944e0e731506380d1526d49b7c6e7cc20cae
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-06-29 02:56:39 +0530

    Time: 18 ms (79.94%), Space: 54.4 MB (9.46%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -6,7 +6,7 @@ class Solution {
         int right = people.length-1;
         
         while(left<=right){
-            if(people[left] + people[right] <=limit){
+            if(people[left] + people[right]<=limit){
                 count++;
                 left++;
                 right--;
