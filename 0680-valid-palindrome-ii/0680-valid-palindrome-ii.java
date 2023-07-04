f67f85dd74e10eca4c807beb2296c1b37f58e1b0
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-07-04 10:36:05 +0530

    Time: 22 ms (8.60%), Space: 44.6 MB (34.71%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,34 +1,41 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int count1= 0;
-        int count2 = 0;
+        
+        if(s.length()<=1){
+            return true;
+        }
+        int count1 = 0;
+        int count2 =0 ;
         int i=0;
         int j = s.length()-1;
+        
         while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
+            if(s.charAt(i) != s.charAt(j)){
+                i++;
+                count1++;
+            }else{
                 i++;
                 j--;
-            }else{
-                count1++;
-                i++;                
             }
         }
         
         i = 0;
         j = s.length()-1;
         
+        
         while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            }else{
+            if(s.charAt(i) != s.charAt(j)){
                 j--;
                 count2++;
+            }else{
+                i++;
+                j--;
             }
         }
-
-        if(count1==1 || count2==1) return true;
-        if(count1==0 || count2==0) return true;
+        
+        
+        if(count1== 1 || count2 == 1) return true;
+        if(count1== 0 || count2 == 0) return true;
         
         return false;
     }
