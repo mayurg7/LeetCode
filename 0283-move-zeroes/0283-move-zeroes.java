7777dfd74fae82099a684de5a8923953aa985390
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-07-05 03:05:24 +0530

    Time: 1 ms (100.00%), Space: 45.3 MB (40.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j =0;
+        int j = 0;
+        
         for(int i=0;i<nums.length;i++){
             if(nums[i] != 0){
                 nums[j] = nums[i];
@@ -8,8 +9,9 @@ class Solution {
             }
         }
         
+        
         while(j<nums.length){
-            nums[j] =0;
+            nums[j] = 0;
             j++;
         }
     }
