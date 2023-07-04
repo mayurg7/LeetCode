6dcff8507217d23c8ffe1ba653121f4ab7ec640b
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-07-04 21:56:17 +0530

    Time: 118 ms (16.19%), Space: 54.8 MB (72.43%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,16 +1,17 @@
 class Solution {
+    
+    // Min Heap
     public int[] sortArray(int[] nums) {
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         for(int i : nums){
             pq.offer(i);
         }
-        
-        int i = 0;
+        int i=0;
         while(!pq.isEmpty()){
-            nums[i++] = pq.poll();
+            nums[i] = pq.poll();
+            i++;
         }
-        
         return nums;
     }
 }
