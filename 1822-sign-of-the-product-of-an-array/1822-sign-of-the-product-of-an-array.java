efc45cff2cc199a87885a6ba8d2e29dfdad7d95a
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-07-06 22:35:53 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (9.47%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        
+        for(int i:nums){
+            if(i==0){
+                return 0;
+            }else if(i<0){
+                product = -product;
+            }
+        }
+        return product;
+        
+    }
+}
