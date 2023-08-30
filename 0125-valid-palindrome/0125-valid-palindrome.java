bdfe9c1abf856c974e6ba18e3713b3ba6a9c1b6b
mayurg7 <116115501+mayurg7@users.noreply.github.com>
2023-08-30 14:30:04 +0530

    Time: 5 ms (55.63%), Space: 44.3 MB (23.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
         s = s.toLowerCase();
-        
+        StringBuilder sb = new StringBuilder();
         
         for(int i=0;i<s.length();i++){
-            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))){
+            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i)) ){
                 sb.append(s.charAt(i));
             }
         }
         
-        
         int j = sb.length()-1;
-        for(int i = 0;i<sb.length();i++){
+        
+        for(int i=0;i<sb.length();i++){
             if(sb.charAt(i) != sb.charAt(j)){
                 return false;
             }else{
@@ -20,6 +19,5 @@ class Solution {
             }
         }
         return true;
-
     }
 }
